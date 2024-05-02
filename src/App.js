2975d47081601a61fe46350f87b2c680f7950ed1
MuhammadFaizan42 <m.faizan42.mf@gmail.com>
2024-05-02 22:38:59 +0500

    banner design

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
 import Layout from './layout';
+import HomePage from './pages/homepage';
 
 function App() {
   return (
     <div className="App">
       <div className="page_width">
           
-      <Layout>Hello world </Layout>;
-      
+      <Layout>
+        <HomePage/> 
+        
+        </Layout>;
+
         </div>
        
     </div>
